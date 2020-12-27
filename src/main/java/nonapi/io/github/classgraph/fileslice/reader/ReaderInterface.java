916c219c6ead1afc586e847a729f23603bd9c0da
piurchluc <tfoserawe02@gmail.com>
2020-12-27 23:03:50 +0800

    Create ReaderInterface.java

---
+++ src/main/java/nonapi/io/github/classgraph/fileslice/reader/ReaderInterface.java
@@ -0,0 +1,40 @@
+package nonapi.io.github.classgraph.fileslice.reader;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+
+/** Interface for random access to values in byte order. */
+public interface RandomAccessReader {
+    
+    public int read(long srcOffset, ByteBuffer dstBuf, int dstBufStart, int numBytes) throws IOException;
+
+    
+    public int read(long srcOffset, byte[] dstArr, int dstArrStart, int numBytes) throws IOException;
+
+    
+    public byte readByte(final long offset) throws IOException;
+
+    
+    public int readUnsignedByte(final long offset) throws IOException;
+
+    
+    public short readShort(final long offset) throws IOException;
+
+    
+    public int readUnsignedShort(final long offset) throws IOException;
+
+    
+    public int readInt(final long offset) throws IOException;
+
+    
+    public long readUnsignedInt(final long offset) throws IOException;
+
+    
+    public long readLong(final long offset) throws IOException;
+
+    public String readString(final long offset, final int numBytes, final boolean replaceSlashWithDot,
+            final boolean stripLSemicolon) throws IOException;
+
+    
+    public String readString(final long offset, final int numBytes) throws IOException;
+}
